8b1bb03ad281df726d2db2fa3be23ddd3e56b5f9
A <A>
2023-09-02 18:54:50 +0300

    Initial Commit

---
+++ ConfectioneryShop/ConfectioneryShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfectioneryShopApp.swift
+//  ConfectioneryShop
+//
+//  Created by Вяткин Александр on 02.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ConfectioneryShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
